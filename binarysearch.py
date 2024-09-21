3c3b41f1183822aa6344f657f6eac1ec30d3dbf7
Pushpa5002 <romeyababar@gmail.com>
2024-09-21 23:25:02 +0530

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,31 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid=(high+low)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            return binary_search(arr, mid + 1, high, x)
+        else:
+            return binary_search(arr, low, mid - 1, x)
+    else:
+        return -1
+
+arr = np.arange(1, 10)
+x = 500
+
+search_times = [12]
+for n in range(10, len(arr) + 1, 10):
+    start_time = time.time()
+    binary_search(arr[:n], 0, n - 1, x)
+    end_time = time.time()
+    search_times.append(end_time - start_time)
+
+plt.plot(search_times)
+plt.xlabel('Array size (n)')
+plt.ylabel('Time (s)')
+plt.title('Time complexity of binary search')
+plt.show()
